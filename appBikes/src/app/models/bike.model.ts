ef484d466af3061ddf723393e53e25f15c99b962
Clément ULRICH <clement.ulrich79@gmail.com>
2026-01-05 10:14:48 +0100

    Modifs modèles vélos + ajout catégories multiples

--- appBikes/src/app/models/bike.model.ts
+++ appBikes/src/app/models/bike.model.ts
@@ -1,8 +1,10 @@
 export interface Bike {
   id: number;
   name: string;
-  category: string;
+  category: string[];
   description: string;
   price: number;
+  brand: string;
+  stock: number;
   imageUrl: string;
 }
